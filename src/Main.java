d5f59de7039a15f753e6bedd67f4e3005bc30c53
Ludmila <ludmila.drapac@cnas.gov.md>
2023-12-05 21:30:19 -1000

    0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import automobil.Automobil;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Automobil skoda = new Automobil(2020, "Skoda Superb");
+        Automobil dacia = new Automobil(2015, "Dacia Logan");
+        Automobil mazda = new Automobil(2017, "Mazda");
+        Automobil renault = new Automobil(2021, "Renault");
+        Automobil volkswagen = new Automobil(2019, "Volkswagen Polo");
+
+        Set<Automobil> automobilSet = new HashSet<>();
+        automobilSet.add(skoda);
+        automobilSet.add(dacia);
+        automobilSet.add(mazda);
+        automobilSet.add(renault);
+        automobilSet.add(volkswagen);
+
+         Set<Automobil> sorted = automobilSet.stream()
+                 .sorted()
+                 .collect(Collectors.toCollection(LinkedHashSet::new));
+        System.out.println("HashSet de automobile care sunt sortate ascendent după anul producerii");
+        System.out.println(sorted);
+
+
+        TreeSet<Automobil> automobilTreeSet = new TreeSet<>();
+        automobilTreeSet.add(skoda);
+        automobilTreeSet.add(dacia);
+        automobilTreeSet.add(mazda);
+        automobilTreeSet.add(renault);
+        automobilTreeSet.add(volkswagen);
+
+        System.out.println("                                                                                       ");
+        System.out.println("TreeSet de automobile care sunt sortate utilizînd un Comparator");
+        System.out.println(automobilTreeSet);
+
     }
 }
--- src/automobil/Automobil.java
+++ src/automobil/Automobil.java
@@ -1,2 +1,52 @@
-package automobil;public class Automobil {
-}
+package automobil;
+
+import java.util.Objects;
+
+public class Automobil implements Comparable<Automobil> {
+
+    private int anProducere;
+    private String denumire;
+
+    public Automobil(int anProducere, String denumire) {
+        this.anProducere = anProducere;
+        this.denumire = denumire;
+    }
+
+    public int getAnProducere() {
+
+        return anProducere;
+    }
+
+    public String getDenumire() {
+        return denumire;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Automobil automobil = (Automobil) o;
+        return anProducere == automobil.getAnProducere();
+    }
+    @Override
+    public int hashCode() {
+        return anProducere;
+    }
+
+    @Override
+    public String toString() {
+        return "Automobil{" +
+                "anProducere=" + anProducere +
+                ", denumire='" + denumire + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Automobil o) {
+        return this.anProducere - o.getAnProducere();
+
+
+
+    }
+
+
+    }
+
